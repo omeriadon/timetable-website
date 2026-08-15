f252bf3c6440e6226e5fdf2513eb944edad29284
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-08-15 18:29:14 +0800

    replace toolbar svg glass with liquid glass react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,32 +23,6 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>
-				<svg aria-hidden="true" width="0" height="0" focusable="false">
-					<filter
-						id="toolbar-button-liquid-glass"
-						x="-20%"
-						y="-20%"
-						width="140%"
-						height="140%"
-					>
-						<feTurbulence
-							type="fractalNoise"
-							baseFrequency="0.012 0.018"
-							numOctaves="1"
-							seed="8"
-							result="noise"
-						/>
-						<feDisplacementMap
-							in="SourceGraphic"
-							in2="noise"
-							scale="10"
-							xChannelSelector="R"
-							yChannelSelector="G"
-							result="displaced"
-						/>
-						<feGaussianBlur in="displaced" stdDeviation="0.25" />
-					</filter>
-				</svg>
 				<ToolbarProvider>
 					<div className={styles.appShell}>
 						<Sidebar />
