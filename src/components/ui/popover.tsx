613258eb675042b0dc6e56f55e9f009306a179cb
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-08-20 19:33:52 +0800

    extract taildinw out of popover

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -4,12 +4,13 @@ import * as React from "react";
 import { Popover as PopoverPrimitive } from "@base-ui/react/popover";
 
 import { cn } from "@/lib/utils";
+import styles from "./popover.module.css";
 
-function Popover({ ...props }: PopoverPrimitive.Root.Props) {
+function Popover(props: PopoverPrimitive.Root.Props) {
 	return <PopoverPrimitive.Root data-slot="popover" {...props} />;
 }
 
-function PopoverTrigger({ ...props }: PopoverPrimitive.Trigger.Props) {
+function PopoverTrigger(props: PopoverPrimitive.Trigger.Props) {
 	return <PopoverPrimitive.Trigger data-slot="popover-trigger" {...props} />;
 }
 
@@ -32,14 +33,11 @@ function PopoverContent({
 				alignOffset={alignOffset}
 				side={side}
 				sideOffset={sideOffset}
-				className="isolate z-50"
+				className={styles.positioner}
 			>
 				<PopoverPrimitive.Popup
 					data-slot="popover-content"
-					className={cn(
-						"z-50 flex w-72 origin-(--transform-origin) flex-col gap-4 rounded-3xl bg-popover p-4 text-sm text-popover-foreground shadow-lg ring-1 ring-foreground/5 outline-hidden duration-100 data-[side=bottom]:slide-in-from-top-2 data-[side=inline-end]:slide-in-from-left-2 data-[side=inline-start]:slide-in-from-right-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:ring-foreground/10 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
-						className,
-					)}
+					className={cn(styles.content, className)}
 					{...props}
 				/>
 			</PopoverPrimitive.Positioner>
@@ -51,7 +49,7 @@ function PopoverHeader({ className, ...props }: React.ComponentProps<"div">) {
 	return (
 		<div
 			data-slot="popover-header"
-			className={cn("flex flex-col gap-1 text-sm", className)}
+			className={cn(styles.header, className)}
 			{...props}
 		/>
 	);
@@ -61,7 +59,7 @@ function PopoverTitle({ className, ...props }: PopoverPrimitive.Title.Props) {
 	return (
 		<PopoverPrimitive.Title
 			data-slot="popover-title"
-			className={cn("text-base font-medium", className)}
+			className={cn(styles.title, className)}
 			{...props}
 		/>
 	);
@@ -74,7 +72,7 @@ function PopoverDescription({
 	return (
 		<PopoverPrimitive.Description
 			data-slot="popover-description"
-			className={cn("text-muted-foreground", className)}
+			className={cn(styles.description, className)}
 			{...props}
 		/>
 	);
