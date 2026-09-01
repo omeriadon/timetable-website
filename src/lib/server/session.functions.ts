ac1c093f689e127ef1aac35469f3a0a08d8cdc02
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-09-01 16:50:49 +0800

    finish server module cleanup

--- src/lib/server/session.functions.ts
+++ src/lib/server/session.functions.ts
@@ -1,11 +1,8 @@
 import { createServerFn } from "@tanstack/react-start";
 export const checkSession = createServerFn({ method: "GET" }).handler(
 	async () => {
-		const {
-			authenticatedPMSTTRequest,
-			clearSession,
-			writeSession,
-		} = await import("@/lib/server/pmstt.server");
+		const { authenticatedPMSTTRequest, clearSession, writeSession } =
+			await import("@/lib/server/pmstt.server");
 		const { response, tokens } = await authenticatedPMSTTRequest("v1/account");
 		if (tokens) {
 			writeSession(tokens);
